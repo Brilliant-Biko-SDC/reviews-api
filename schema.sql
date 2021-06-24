61aa214934917d00198531c8571757c30aaaf208
Hollin Wakefield <45081384+hollinwakefield@users.noreply.github.com>
2021-06-24 08:39:39 -0700

    Created tables

---
+++ schema.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS ratingAndReviews;
+
+CREATE DATABASE ratingAndReviews;
+
+DROP TABLE IF EXISTS Product;
+
+DROP TABLE IF EXISTS Reviews;
+
+DROP TABLE IF EXISTS Review;
+
+DROP TABLE IF EXISTS Helpfulness;
+
+DROP TABLE IF EXISTS Photos;
+
+DROP TABLE IF EXISTS Characteristics_Product;
+
+DROP TABLE IF EXISTS Characteristics_Review;
+
+CREATE TABLE Product (
+
+);
+
+CREATE TABLE Reviews (
+
+);
+
+CREATE TABLE Review (
+
+);
+
+CREATE TABLE Helpfulness (
+
+);
+
+CREATE TABLE Photos (
+
+);
+
+CREATE TABLE Characteristics_Product (
+
+);
+
+CREATE TABLE Characteristics_Review (
+
+);
